f80e88661b2d72faaa6f7b6d07d0cafccbc3ee8b
Christopher Lee <890136@swansea.ac.uk>
2020-06-26 16:38:07 +0800

    removed uncessary pages.

--- src/layout/dashboard/DashboardLayout.vue
+++ src/layout/dashboard/DashboardLayout.vue
@@ -3,12 +3,12 @@
     <side-bar>
       <template slot="links">
         <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
-        <sidebar-link to="/stats" name="User Profile" icon="ti-user"/>
+        <!-- <sidebar-link to="/stats" name="User Profile" icon="ti-user"/> -->
         <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt"/>
         <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
         <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
-        <sidebar-link to="/maps" name="Map" icon="ti-map"/>
-        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
+        <!-- <sidebar-link to="/maps" name="Map" icon="ti-map"/> -->
+        <!-- <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/> -->
       </template>
       <mobile-menu>
         <li class="nav-item">
@@ -37,7 +37,7 @@
       </mobile-menu>
     </side-bar>
     <div class="main-panel">
-      <top-navbar></top-navbar>
+      <!-- <top-navbar></top-navbar> -->
 
       <dashboard-content @click.native="toggleSidebar">
 
